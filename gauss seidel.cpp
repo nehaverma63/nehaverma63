085cb16cbf30961f4b279e78383c69bade3fb101
nehaverma63 <161279532+nehaverma63@users.noreply.github.com>
2025-05-04 22:54:25 +0530

    Create gauss seidel.cpp

---
+++ gauss seidel.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    const int n = 3; // Number of equations
+    float a[n][n + 1], x[n] = {0}, old_x[n];
+    float tol = 0.001;
+    int max_iter = 100, iter = 0;
+
+    cout << "Enter the augmented matrix (3x4):\n";
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j <= n; j++)
+            cin >> a[i][j];
+
+    do {
+        for (int i = 0; i < n; i++)
+            old_x[i] = x[i];
+
+        for (int i = 0; i < n; i++) {
+            float sum = a[i][n];
+            for (int j = 0; j < n; j++) {
+                if (j != i)
+                    sum -= a[i][j] * x[j];
+            }
+            x[i] = sum / a[i][i];
+        }
+
+        // Check for convergence
+        float err = 0;
+        for (int i = 0; i < n; i++)
+            err += fabs(x[i] - old_x[i]);
+
+        if (err < tol)
+            break;
+
+        iter++;
+    } while (iter < max_iter);
+
+    cout << "Solutions after " << iter << " iterations:\n";
+    for (int i = 0; i < n; i++)
+        cout << "x" << i + 1 << " = " << x[i] << endl;
+
+    return 0;
+}
